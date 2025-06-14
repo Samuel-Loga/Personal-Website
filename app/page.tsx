16db2ce1a70337a32f3e90173a641e565bd1ab2a
Samuel Loga <samuelloga9@gmail.com>
2025-06-14 09:19:40 +0200

    Misc Updates

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <About />
-      <Certifications /> 
+      <Certifications />
       <Experience />
       <Skills />
       <Projects />  
